d1ba200b2fff96c4fad77fea4bdeb92cc33080e7
oskar-anderson <andersonkarloskar@gmail.com>
2022-05-30 21:56:02 +0300

    [save] test monogame vs sfml draw performance

--- Battleship/MonoGame/Game1.cs
+++ Battleship/MonoGame/Game1.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -7,16 +9,22 @@ namespace MonoGame
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
-        private SpriteBatch? _spriteBatch;
-
+        private SpriteBatch _spriteBatch = default!;
+        private SpriteFont _spriteFont = default!;
+        private int frameCount = 0;
+        
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             _graphics.IsFullScreen = false;
-            _graphics.PreferredBackBufferHeight = 480;
-            _graphics.PreferredBackBufferWidth = 640;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.PreferredBackBufferWidth = 960;
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            
+            _graphics.SynchronizeWithVerticalRetrace = false;
+            IsFixedTimeStep = true;
+            TargetElapsedTime = TimeSpan.FromMilliseconds(1000.0f / 1000);
         }
 
         protected override void Initialize()
@@ -29,7 +37,7 @@ namespace MonoGame
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            _spriteFont = Content.Load<SpriteFont>("builtContent/PressStart2pFont");
             // TODO: use this.Content to load your game content here
         }
 
@@ -46,7 +54,24 @@ namespace MonoGame
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            var width = 40;
+            var height = 40;
+            var chars = "zxcasdqwe";
+            var random = new Random();
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    _spriteBatch.Begin();
+                    _spriteBatch.DrawString(_spriteFont, chars[random.Next() % chars.Length].ToString(), new Vector2(x * 16, y * 16), Color.White);
+                    _spriteBatch.End();
+                }
+            }
 
+            frameCount++;
+            _spriteBatch.Begin();
+            _spriteBatch.DrawString(_spriteFont, $"FrameCount: {frameCount}", new Vector2(40, 700), Color.White);
+            _spriteBatch.End();
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
---
+++ Battleship/SfmlDrawPerformance/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Numerics;
+using System.Text;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+// this is just a quick and dirty test - you have to place the font manually
+Font font = new Font(AppDomain.CurrentDomain.BaseDirectory + "/PressStart2P.ttf");
+var window = new RenderWindow(new VideoMode(960, 720), "Battleships");
+window.Closed += (sender, e) =>
+{
+    window.Close();
+};
+window.SetFramerateLimit(0);
+Console.OutputEncoding = Encoding.Unicode;
+var width = 40;
+var height = 40;
+var chars = "zxcasdqwe";
+var random = new Random();
+var frameCount = 0;
+while (true)
+{
+    // fps: 200
+    window.Clear();
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            var text = new Text(
+                chars[random.Next() % chars.Length].ToString(), 
+                font)
+            {
+                Position = new Vector2f(
+                    x * 16, 
+                    y * 16), 
+                CharacterSize = 16
+            };
+            window.Draw(text);
+        }
+    }
+
+    frameCount++;
+    window.Draw(new Text($"FrameCount: {frameCount}", font) 
+    {
+        Position = new Vector2f(40, 700),
+        CharacterSize = 16
+    });
+    window.Display();
+}
+
